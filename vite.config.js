2acc7136d5f20d9e98c0bcea4c7aca52e42f8d90
Nho-way <nickho.lv@gmail.com>
2024-09-26 18:24:20 -0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,9 +6,11 @@ export default defineConfig({
         rollupOptions: {
             input: {
                 'about-us': resolve(__dirname, 'pages/about-us/index.html'),
-                instruction: resolve(__dirname, 'pages/instructions/index.html'),
+                'instruction': resolve(__dirname, 'pages/instructions/index.html'),
                 'select-cards': resolve(__dirname, 'pages/select-cards/index.html'),
-                '8-ball-intro': resolve(__dirname, 'pages/8-ball-intro/index.html'), // Add this line
+                '8-ball-intro': resolve(__dirname, 'pages/8-ball-intro/index.html'), 
+                'about-us-8-ball': resolve(__dirname, 'pages/about-us1/index.html'),
+                'result': resolve(__dirname, 'pages/result-page/index.html'),
                 main: resolve(__dirname, 'index.html'),
             },
         },
